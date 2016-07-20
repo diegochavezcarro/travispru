fb4228e2560ffb3fc014b02968d4d20bfd7fb751
Diego Chavez <diegochavezcarro@gmail.com>
2016-07-20 15:56:06 -0300

    puerto

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
 var http = require('http');
 console.log("puerto en uso");
-console.log(process.env.PORT);
+console.log("puerto: " + process.env.PORT);
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Travis!\n'); // this will FAIL travis ci lint
-}).listen(80, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:80/');
+}).listen(process.env.PORT, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:' + process.env.PORT);
