4c8659b5431bc1d8888a7dff16bb87562c800d96
Diego Chavez <diegochavezcarro@gmail.com>
2016-07-20 16:30:34 -0300

    covertura

--- hello.js
+++ hello.js
@@ -3,6 +3,6 @@ console.log("puerto en uso");
 console.log("puerto: " + process.env.PORT);
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Travis!\n'); // this will FAIL travis CI lint
+    res.end('Hello Travis!!!\n'); // this will FAIL travis CI lint
 }).listen(process.env.PORT);
 console.log('Server running at http://127.0.0.1:' + process.env.PORT);
