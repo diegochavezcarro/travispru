b2aaf4b4694f3d6ac6fa48fd4c265a0d2d70ac46
Diego Chavez <diegochavezcarro@gmail.com>
2016-08-08 11:03:07 -0300

    punto coma

--- hello.js
+++ hello.js
@@ -4,5 +4,5 @@ console.log("puerto: " + process.env.PORT);
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Travis!\n'); // this will FAIL travis CI lint
-}).listen(process.env.PORT)
+}).listen(process.env.PORT);
 console.log('Server running at http://127.0.0.1:' + process.env.PORT);
