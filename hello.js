9796e01b9c7bde622ea238fa07f31ce28b465c74
Diego Chavez <diegochavezcarro@gmail.com>
2016-08-09 11:44:54 -0300

    sss

--- hello.js
+++ hello.js
@@ -1,8 +1,14 @@
 var http = require('http');
+var index = require('.index');
 console.log("puerto en uso");
 console.log("puerto: " + process.env.PORT);
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Travis!\n'); // this will FAIL travis CI lint
+    var memdb = index.clear();
+    var tobi = { name: 'Tobi' };
+    memdb.save(tobi);
+    var ret = memdb.first({ name: 'Tobi' });
+    var welcome = 'Hello Travis!\n' + ret;
+    res.end(welcome); // this will FAIL travis CI lint
 }).listen(process.env.PORT);
 console.log('Server running at http://127.0.0.1:' + process.env.PORT);
