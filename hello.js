700dd42915672ad38b5a91696634603974d52250
Diego Chavez <diegochavezcarro@gmail.com>
2016-12-12 12:51:24 -0300

    b

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ var port     = process.env.PORT || 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     memdb.clear();
-    var tobi = { name: 'Tobi' }
+    var tobi = { name: 'Tobi' };
     memdb.save(tobi);
     var ret = memdb.first({ name: 'Tobi' });
     var welcome = 'Hello Travis\n' + ret.name;
