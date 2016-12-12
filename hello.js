326b81f6a3482f720541bf922ebe81bc9d77db25
Diego Chavez <diegochavezcarro@gmail.com>
2016-12-12 12:48:49 -0300

    punto y coma

--- hello.js
+++ hello.js
@@ -4,10 +4,10 @@ var port     = process.env.PORT || 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     memdb.clear();
-    var tobi = { name: 'Tobi' };
+    var tobi = { name: 'Tobi' }
     memdb.save(tobi);
     var ret = memdb.first({ name: 'Tobi' });
-    var welcome = 'Hello Travis!!!!!!\n' + ret.name;
+    var welcome = 'Hello Travis\n' + ret.name;
     res.end(welcome); 
 }).listen(port);
 console.log('Server running at http://localhost:' + port);
