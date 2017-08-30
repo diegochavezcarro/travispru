1a9e2b17ec3b40d7f7f33a2d57d2430d515d060f
Diego Chavez <diegochavezcarro@gmail.com>
2017-08-30 09:57:48 -0300

    eval

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ var port     = process.env.PORT || 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     memdb.clear();
-    var tobi = { name: 'Travis' };
+    var tobi = eval({ name: 'Travis' });
     memdb.save(tobi);
     var ret = memdb.first({ name: 'Travis' });
     var welcome = 'Hello ' + ret.name + '!';
